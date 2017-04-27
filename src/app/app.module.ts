4b006e7c2868bb509993fed0e6f636b11e0650cf
= <prianikovkirill@gmail.com>
2017-04-27 17:20:46 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HeroService } from './hero.service';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@NgModule({
+  imports:      [ 
+                  BrowserModule,
+                  FormsModule
+                ],
+  declarations: [ 
+                  AppComponent,
+                  HeroDetailComponent
+                ],
+  providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
